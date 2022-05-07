42121d75e10d9f858f3a91b6a39f5722c02868f0
Xueyan Zou <xueyanzoucs@gmail.com>
2022-05-07 00:28:53 -0500

    Update to reduce logger info on cuda oom

--- detectron2/utils/memory.py
+++ detectron2/utils/memory.py
@@ -76,7 +76,7 @@ def retry_if_cuda_oom(func):
 
         # Try on CPU. This slows down the code significantly, therefore print a notice.
         logger = logging.getLogger(__name__)
-        logger.info("Attempting to copy inputs of {} to CPU due to CUDA OOM".format(str(func)))
+        logger.info("Attempting to copy inputs of {} to CPU due to CUDA OOM".format(str(func)[0:5]))
         new_args = (maybe_to_cpu(x) for x in args)
         new_kwargs = {k: maybe_to_cpu(v) for k, v in kwargs.items()}
         return func(*new_args, **new_kwargs)
